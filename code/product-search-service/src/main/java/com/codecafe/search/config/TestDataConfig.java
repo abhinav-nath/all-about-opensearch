91ebe0054e41e6c91c7f0bffa75a6787e6cd2743
Abhinav Nath <abhinavnath@ymail.com>
2022-01-04 14:44:05 +0530

    Create product documents during app start up

--- code/product-search-service/src/main/java/com/codecafe/search/config/TestDataConfig.java
+++ code/product-search-service/src/main/java/com/codecafe/search/config/TestDataConfig.java
@@ -2,16 +2,15 @@ package com.codecafe.search.config;
 
 import com.codecafe.search.document.ProductDocument;
 import com.codecafe.search.repository.ProductRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.data.elasticsearch.core.completion.Completion;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
 
 import javax.annotation.PostConstruct;
-import java.math.BigDecimal;
-import java.time.ZonedDateTime;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Component
@@ -21,53 +20,23 @@ public class TestDataConfig {
     private static final org.slf4j.Logger logger = LoggerFactory.getLogger(TestDataConfig.class);
 
     private final ProductRepository productRepository;
+    private ObjectMapper objectMapper;
 
     @Autowired
-    public TestDataConfig(ProductRepository productRepository) {
+    public TestDataConfig(ProductRepository productRepository, ObjectMapper objectMapper) {
         this.productRepository = productRepository;
+        this.objectMapper = objectMapper;
     }
 
     @PostConstruct
     public void init() {
         try {
             productRepository.deleteAll();
-            List<ProductDocument> productDocuments = buildProductDocuments();
+            List<ProductDocument> productDocuments = Arrays.asList(objectMapper.readValue(ResourceUtils.getFile("classpath:" + "product_catalog/products.json"), ProductDocument[].class));
             productRepository.saveAll(productDocuments);
         } catch (Exception ex) {
             logger.error("Failed to create test data. Reason : {}", ex.getMessage());
         }
-    }
-
-    private List<ProductDocument> buildProductDocuments() {
-        List<ProductDocument> productDocuments = new ArrayList<>();
-
-        productDocuments.add(ProductDocument.builder()
-                .id("1")
-                .name("Macbook Pro 2019")
-                .brand("Apple")
-                .categories(List.of("Laptops", "Productivity Laptops"))
-                .description("Apple Macbook Pro 2019 model")
-                .suggest(new Completion(List.of("macbook", "pro", "productivity", "laptops", "laptop")))
-                .color("Silver")
-                .price(BigDecimal.valueOf(2200))
-                .dateAdded(ZonedDateTime.now())
-                .isInStock(true)
-                .build());
-
-        productDocuments.add(ProductDocument.builder()
-                .id("2")
-                .name("ROG Zephyrus")
-                .brand("Asus")
-                .categories(List.of("Laptops", "Gaming Laptops"))
-                .description("Asus ROG Zephyrus Ryzen 9")
-                .suggest(new Completion(List.of("asus", "rog", "zephyrus", "ryzen", "gaming")))
-                .color("Black")
-                .price(BigDecimal.valueOf(1550))
-                .dateAdded(ZonedDateTime.now())
-                .isInStock(true)
-                .build());
-
-        return productDocuments;
     }
 
 }
