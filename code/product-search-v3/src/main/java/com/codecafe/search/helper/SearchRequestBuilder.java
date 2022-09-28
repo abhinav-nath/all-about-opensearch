e88d028ac4d68a0d434708088d5c59903d55868d
Abhinav Nath <abhinavnath@ymail.com>
2022-09-29 00:01:18 +0530

    Add Wildcard Query

--- code/product-search-v3/src/main/java/com/codecafe/search/helper/SearchRequestBuilder.java
+++ code/product-search-v3/src/main/java/com/codecafe/search/helper/SearchRequestBuilder.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.opensearch.client.opensearch._types.FieldValue;
 import org.opensearch.client.opensearch._types.query_dsl.MatchQuery;
 import org.opensearch.client.opensearch._types.query_dsl.Query;
+import org.opensearch.client.opensearch._types.query_dsl.WildcardQuery;
 import org.opensearch.client.opensearch.core.SearchRequest;
 import org.springframework.stereotype.Component;
 
@@ -22,13 +23,18 @@ public class SearchRequestBuilder {
 
   public SearchRequest buildTextSearchRequest(String searchText, List<FacetData> facets, int page, int size) {
 
-    Query byName = MatchQuery.of(m -> m
+    Query matchName = MatchQuery.of(m -> m
       .field("name")
       .query(FieldValue.of(searchText))
     )._toQuery();
 
+    Query matchNameWildcard = WildcardQuery.of(m -> m
+      .field("name")
+      .value("*" + searchText + "*")
+    )._toQuery();
+
     return new SearchRequest.Builder()
-      .query(q -> q.bool(b -> b.should(byName)))
+      .query(q -> q.bool(b -> b.should(matchName).should(matchNameWildcard)))
       .build();
   }
 
