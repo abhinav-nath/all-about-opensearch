1ab1fb9474a9ff3164560c80481774934408ff77
Abhinav Nath <abhinavnath@ymail.com>
2022-09-30 21:45:27 +0530

    Add boost on query fields

--- code/product-search-v3/src/main/java/com/codecafe/search/helper/SearchRequestBuilder.java
+++ code/product-search-v3/src/main/java/com/codecafe/search/helper/SearchRequestBuilder.java
@@ -1,12 +1,14 @@
 package com.codecafe.search.helper;
 
 import java.util.List;
+import java.util.Map;
 
 import org.opensearch.client.opensearch._types.FieldValue;
 import org.opensearch.client.opensearch._types.query_dsl.MatchQuery;
 import org.opensearch.client.opensearch._types.query_dsl.Query;
 import org.opensearch.client.opensearch._types.query_dsl.WildcardQuery;
 import org.opensearch.client.opensearch.core.SearchRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
@@ -21,21 +23,30 @@ public class SearchRequestBuilder {
   private final FacetsBuilder facetsBuilder;
   private final OpenSearchConfiguration openSearchConfiguration;
 
+  @Value("#{${app.search.query-boost-fields}}")
+  private Map<String, Float> queryBoostFields;
+
   public SearchRequest buildTextSearchRequest(String searchText, List<FacetData> facets, int page, int size) {
 
-    Query matchName = MatchQuery.of(m -> m
-      .field("name")
-      .query(FieldValue.of(searchText))
+    Query matchCode = MatchQuery.of(m -> m.field("code")
+                                          .query(FieldValue.of(searchText))
+                                          .boost(queryBoostFields.get("codeBoost"))
     )._toQuery();
 
-    Query matchNameWildcard = WildcardQuery.of(m -> m
-      .field("name")
-      .value("*" + searchText + "*")
+    Query matchName = MatchQuery.of(m -> m.field("name")
+                                          .query(FieldValue.of(searchText))
+                                          .boost(queryBoostFields.get("nameBoost"))
     )._toQuery();
 
-    return new SearchRequest.Builder()
-      .query(q -> q.bool(b -> b.should(matchName).should(matchNameWildcard)))
-      .build();
+    Query matchNameWildcard = WildcardQuery.of(m -> m.field("name")
+                                                     .boost(queryBoostFields.get("wildcardBoost"))
+                                                     .value("*" + searchText + "*")
+    )._toQuery();
+
+    return new SearchRequest.Builder().query(q -> q.bool(b -> b.should(matchCode)
+                                                               .should(matchName)
+                                                               .should(matchNameWildcard)))
+                                      .build();
   }
 
 }
