08a6e8f5bba2ffb399b7b9ad2c6e71b2457bb993
Abhinav Nath <abhinavnath@ymail.com>
2022-10-22 19:44:14 +0530

    Update db migration sql

--- code/product-search-v3/src/main/resources/db/migration/V01__Create_Facets_Config_table.sql
+++ code/product-search-v3/src/main/resources/db/migration/V01__Create_Facets_Config_table.sql
@@ -1,17 +1,15 @@
 create table IF NOT EXISTS facets_config
 (
-  code             VARCHAR(255) PRIMARY KEY,
-  display_name     VARCHAR(255) NOT NULL,
-  sequence         INTEGER,
-  is_measurement   BOOLEAN,
-  measurement_units JSONB
+  code         VARCHAR(255) PRIMARY KEY,
+  display_name VARCHAR(255) NOT NULL,
+  sequence     INTEGER
 );
 
-INSERT INTO facets_config VALUES ('price', 'Price', 4, false, null);
-INSERT INTO facets_config VALUES ('color', 'Color', 2, false, null);
-INSERT INTO facets_config VALUES ('department', 'Department', 3, false, null);
-INSERT INTO facets_config VALUES ('brand', 'Brand', 1, false, null);
-INSERT INTO facets_config VALUES ('length', 'Length', 5, true, '{"default": "inches", "metric": "centimetres", "imperial": "inches"}');
-INSERT INTO facets_config VALUES ('width', 'Width', 6, true, '{"default": "inches", "metric": "centimetres", "imperial": "inches"}');
-INSERT INTO facets_config VALUES ('height', 'Height', 7, true, '{"default": "inches", "metric": "centimetres", "imperial": "inches"}');
-INSERT INTO facets_config VALUES ('weight', 'Weight', 8, true, '{"default": "pounds", "metric": "kilograms", "imperial": "pounds"}');
+INSERT INTO facets_config
+VALUES ('color', 'Color', 2);
+
+INSERT INTO facets_config
+VALUES ('department', 'Department', 3);
+
+INSERT INTO facets_config
+VALUES ('brand', 'Brand', 1);
