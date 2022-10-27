42cdb82c46577fad62066b062c8b8bb2a88989a5
Abhinav Nath <abhinavnath@ymail.com>
2022-10-27 23:55:55 +0530

    Set fullText and fullTextBoosted fields

--- code/product-search-v3/src/main/java/com/codecafe/search/config/TestDataConfiguration.java
+++ code/product-search-v3/src/main/java/com/codecafe/search/config/TestDataConfiguration.java
@@ -60,6 +60,8 @@ public class TestDataConfiguration {
     productDocument.setCreatedAt(Instant.now().toEpochMilli());
     productDocument.setModifiedAt(Instant.now().toEpochMilli());
 
+    productDocument.setFullTexts();
+
     String productDocumentStr = objectMapper.writeValueAsString(productDocument);
 
     indexRequest.source(productDocumentStr, JSON);
--- code/product-search-v3/src/main/java/com/codecafe/search/document/ProductDocument.java
+++ code/product-search-v3/src/main/java/com/codecafe/search/document/ProductDocument.java
@@ -1,6 +1,9 @@
 package com.codecafe.search.document;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -12,6 +15,8 @@ public class ProductDocument {
   private String code;
   private String name;
   private String description;
+  private String fullText;
+  private String fullTextBoosted;
   private String brand;
   private String color;
   private String department;
@@ -19,5 +24,20 @@ public class ProductDocument {
   private Boolean inStock;
   private Long createdAt;
   private Long modifiedAt;
+
+  public void setFullTexts() {
+    List<String> fullTextList = new ArrayList<>();
+    fullTextList.add(this.description);
+    fullTextList.add(this.department);
+    this.fullText = fullTextList.stream().collect(Collectors.joining(" "));
+
+    List<String> fullTextBoostedList = new ArrayList<>();
+
+    fullTextBoostedList.add(this.name);
+    fullTextBoostedList.add(this.brand);
+    fullTextBoostedList.add(this.color);
+
+    this.fullTextBoosted = fullTextBoostedList.stream().collect(Collectors.joining(" "));
+  }
 
 }
